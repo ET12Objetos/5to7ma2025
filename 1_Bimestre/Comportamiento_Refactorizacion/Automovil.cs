372bb934aa795def139c39495d5731cb4baedadf
jvelazquez <jonathanvmgs@gmail.com>
2025-04-04 11:39:13 -0300

    Remove comments

--- 1_Bimestre/Comportamiento_Refactorizacion/Automovil.cs
+++ 1_Bimestre/Comportamiento_Refactorizacion/Automovil.cs
@@ -2,12 +2,10 @@ namespace Comportamiento_Refactorizacion;
 
 public class Automovil
 {
-    //atributos
     public string Patente = "";
     public string Modelo = string.Empty;
     public bool EstadoMotor;
 
-    //método constructor
     public Automovil(string Patente, string Modelo)
     {
         Validaciones.Cadena(Patente, "la patente es incorrecta");
@@ -19,7 +17,6 @@ public class Automovil
         this.EstadoMotor = false;
     }
 
-    //compotamiento de encendido y apagado del automovil
     public void Arrancar()
     {
         if (EstadoMotor == false)
